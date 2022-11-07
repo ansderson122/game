24ba73d24310b2b91ce589ae603a9e2991493206
Julyanderson Alves <ansderson84001@gmail.com>
2022-11-07 19:04:48 -0300

    menu iniciado

--- game.py
+++ game.py
@@ -7,23 +7,19 @@ tela = pygame.display.set_mode((800,400))
 pygame.display.set_caption('Meu Joquin')
 relogio = pygame.time.Clock()
 
-
-
-
 level = level(tela)
-game_active = level.game_active
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        if not game_active:    
+        if not level.game_active:    
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-                game_active = True
+                level.game_active = True
                 level.caracol.rect.left = 800
                 tempoInicial = int((pygame.time.get_ticks())/1000)
-    if game_active:
+    if level.game_active:
         level.run()
     
     
---
+++ menu.py
@@ -0,0 +1,5 @@
+import pygame
+
+class menu():
+    def __init__(self) -> None:
+        pass
