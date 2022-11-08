3983073be0809239285ab73f4bff28ca85b2ca07
Julyanderson Alves <ansderson84001@gmail.com>
2022-11-07 21:15:49 -0300

    botao inicia concluido

--- menu.py
+++ menu.py
@@ -4,7 +4,10 @@ class menu():
     def __init__(self,surface):
         self.fonte = pygame.font.Font(None, 25)
         self.surface = surface
-        self.active = True        
+        self.active = True 
+        self.selecaoDificudade = False
+
+        
 
     def botaoInicia(self):
         self.inicia = pygame.Surface((150,50))
@@ -18,5 +21,18 @@ class menu():
         self.surface.blit(self.inicia,self.iniciaRect)
         self.surface.blit(score_surf, score_rect)
 
+    def clickInicia(self):
+        self.mouse = pygame.mouse.get_pos() 
+        event = pygame.event.wait()
+
+        if event.type == pygame.MOUSEBUTTONDOWN and pygame.mouse.get_pressed()[0] :
+            if  (self.mouse[0] >= 350 and self.mouse[0] <= 475) and (self.mouse[1] >= 150 and self.mouse[1] <= 200):
+                self.selecaoDificudade = True
+
+
     def draw(self):
-        self.botaoInicia()
+        self.clickInicia()
+        if not self.selecaoDificudade:
+            self.botaoInicia()
+        else:
+            pass
