5f1206683ba7d8a4828960542d94296fb134ec85
Julyanderson Alves <ansderson84001@gmail.com>
2022-11-09 09:04:05 -0300

    botao selecao de dificudade

--- menu.py
+++ menu.py
@@ -1,25 +1,16 @@
 import pygame
 
-class menu():
+class menu:
     def __init__(self,surface):
         self.fonte = pygame.font.Font(None, 25)
         self.surface = surface
         self.active = True 
         self.selecaoDificudade = False
 
+        self.botaoSelecaoDificudade = selecaoDificudade()
+        self.botaoInicia = botaoInicia()
         
 
-    def botaoInicia(self):
-        self.inicia = pygame.Surface((150,50))
-        self.inicia.fill('white')
-        self.iniciaRect = self.inicia.get_rect(topleft = (325,150))
-
-        score_surf = self.fonte.render('Inicia o jogo', False, (0,0,0))
-        score_rect = score_surf.get_rect(topleft = (350,170))
-        
-        
-        self.surface.blit(self.inicia,self.iniciaRect)
-        self.surface.blit(score_surf, score_rect)
 
     def clickInicia(self):
         self.mouse = pygame.mouse.get_pos() 
@@ -33,6 +24,51 @@ class menu():
     def draw(self):
         self.clickInicia()
         if not self.selecaoDificudade:
-            self.botaoInicia()
+            self.botaoInicia.draw(self.surface)
         else:
-            pass
+            self.botaoSelecaoDificudade.draw(self.surface)
+
+
+
+
+class botaoInicia:
+    def __init__(self):
+        self.fonte =  pygame.font.Font(None, 25)
+
+
+    def draw(self,surface):
+        self.inicia = pygame.Surface((150,50))
+        self.inicia.fill('white')
+        self.iniciaRect = self.inicia.get_rect(topleft = (325,150))
+
+        score_surf = self.fonte.render('Inicia o jogo', False, (0,0,0))
+        score_rect = score_surf.get_rect(topleft = (350,170))
+        
+        
+        surface.blit(self.inicia,self.iniciaRect)
+        surface.blit(score_surf, score_rect)
+
+class selecaoDificudade:
+    def __init__(self):
+        self.fonte =  pygame.font.Font(None, 25)
+
+    def draw(self,surface):
+        self.normalCaixa = pygame.Surface((150,50))
+        self.normalCaixa .fill('white')
+        self.normalRect = self.normalCaixa .get_rect(topleft = (325,150))
+
+        self.dificilCaixa = pygame.Surface((150,50))
+        self.dificilCaixa.fill('white')
+        self.dificilRect = self.dificilCaixa.get_rect(topleft = (325,230))
+
+
+        surface.blit(self.normalCaixa ,self.normalRect)
+        surface.blit(self.dificilCaixa,self.dificilRect)
+
+        score_surf = self.fonte.render('Normal', False, (0,0,0))
+        score_rect = score_surf.get_rect(topleft = (350,170))       
+        surface.blit(score_surf, score_rect)        
+        
+        score_surf1 = self.fonte.render('Difícil', False, (0,0,0))
+        score_rect1 = score_surf1.get_rect(topleft = (350,250))
+        surface.blit(score_surf1, score_rect1)
